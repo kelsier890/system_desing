d9c8cee3b220ce93bf9dc99596dfd82d3d361bba
Jeronimo <jeromonto890@gmail.com>
2024-05-18 11:50:07 -0500

    Docker para el Gateway

--- src/gateway/storage/util.py
+++ src/gateway/storage/util.py
@@ -18,6 +18,7 @@ def upload(f, fs, channel, access):
             properties=pika.BasicProperties(
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
-
-
         ) 
+    except:
+        fs.delete(fid)
+        return "internal server error", 500
