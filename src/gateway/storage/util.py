56957b4bb3a7041e6b8a4a23b836a3554afc5e28
Jeronimo <jeromonto890@gmail.com>
2024-05-18 11:39:52 -0500

    Uso de rabit como distrubuidor

---
+++ src/gateway/storage/util.py
@@ -0,0 +1,23 @@
+import pika, json
+def upload(f, fs, channel, access):
+    try: 
+        fid = fs.put(t)
+    except Exception as err:
+        return "internal server error", 400
+    message = {
+        "video_fid": str(fid),
+        "mp3_fid": None,
+        "username": access ["username"],
+    }
+
+    try:
+        channel.basic_publish(
+            exchange = "",
+            routhing_key="video",
+            body=json.dups(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+
+
+        ) 
