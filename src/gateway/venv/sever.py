2b7cf22cccb90f7607d5c33e062a6b7b46c2b39d
Jeronimo <jeromonto890@gmail.com>
2024-05-18 10:23:10 -0500

    Impletencaion de MONGODB

---
+++ src/gateway/venv/sever.py
@@ -0,0 +1,20 @@
+import os, gridfs, pika, json
+from flask import Flask, request
+from flask_pymongo import PYMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb:/host.minikube.internal:27017/videos"
+
+mongo = PYMongo(server)
+
+fs = gridfs.GridFs(mongo.db)
+
+conection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access,login(request)
